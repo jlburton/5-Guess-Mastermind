f3300f2d4f1787086ed9314c34f1f1f2e4ae4e27
Jason <jeterprimary@gmail.com>
2023-09-13 18:41:53 -0300

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -17,7 +17,6 @@ guesses_made = 0
 def print_set_size(s):
   print(f'Size of the set of possible guesses: {len(s)}')
 
-
 def is_valid_secret(s):
   length = len(s)
   secret = int(s)
@@ -26,17 +25,14 @@ def is_valid_secret(s):
   else:
     return True
 
-
 def convert_to_tuple_of_ints(secret_code):
   secret_code = tuple(map(int, list(secret_code)))
   return secret_code
-
 
 def get_all_codes(DIGITS_USED_IN_CODE):
   digits = tuple(range(DIGITS_USED_IN_CODE))
   all_codes = set(itertools.product(digits, repeat=CODE_LENGTH))
   return all_codes
-
 
 def get_feedback(guess, secret_code):
   feedback = []
@@ -44,7 +40,6 @@ def get_feedback(guess, secret_code):
   secret_code_remaining = []
   if guess == secret_code:
     feedback = FOUR_PLUS_SIGNS
-    #print(feedback)
   else:
     for guess, secret in zip(guess, secret_code):
       if guess == secret:
@@ -60,18 +55,15 @@ def get_feedback(guess, secret_code):
         feedback.append(' ')
   return feedback
 
-
 def rmv(guess, secret_code, set_of_possible_secrets):
   for possible_guess in set_of_possible_secrets.copy():
     if set(get_feedback(possible_guess, secret_code)) != set(
         get_feedback(guess, secret_code)):
       set_of_possible_secrets.remove(possible_guess)
-      #print(get_feedback(guess, secret_code))
-
-
+      
 def make_feedback_dict():
   '''
-  Stores the feedback for each possible guess/code pair.
+  Stores the feedback for each possible guess/secret_code pair.
   '''
   feedback_dict = collections.defaultdict(dict)
   for guess, secret_code in itertools.product(all_codes, repeat=2):
